068c3b65272eedb0fd271f90daa31701c69a153d
Trainwreckadventures <mari.n.depuis@gmail.com>
2024-12-04 16:06:19 +0100

    Worked on the api logic, added users, dropping the .env for now, confirmed api fetch of users in dashboard

--- dynamic_cv_app/README.md
+++ dynamic_cv_app/README.md
@@ -1,7 +1,3 @@
 # Dynamic_CV
 
 "Eksamensoppgave i emne 6. Oppgaven går ut på å utvikle en webapplikasjon som lar brukere opprette, redigere, og tilpasse CV-er direkte gjennom et dynamisk skjema"
-
-# Note: This .env file is committed for simplicity during development.
-
-# It was originally in the .gitignore, but since this api is temporary I am keeping it here for simplicity
---
+++ dynamic_cv_app/src/utils/types.ts
@@ -0,0 +1,21 @@
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+export interface CV {
+  _id: string;
+  user: string;
+  personalInfo: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  skills: string[];
+  experience: { title: string; company: string; years: string }[];
+  education: { degree: string; institution: string; year: string }[];
+  references: { name: string; contactInfo: string }[];
+}
