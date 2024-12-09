c90a0316da4b5338dd2a88f35e2abc7c134151ff
Trainwreckadventures <mari.n.depuis@gmail.com>
2024-12-09 17:58:09 +0100

    Endret på CreateCV.tsx og flyttet sjekken for cv over til CreateCVPage.tsx

--- dynamic_cv_app/src/pages/CreateCVPage.tsx
+++ dynamic_cv_app/src/pages/CreateCVPage.tsx
@@ -3,18 +3,39 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { Navigate } from "react-router-dom";
 import CreateCV from "../features/cv/CreateCV";
+import { useGetCvsQuery } from "../services/api";
 
 const CreateCVPage = () => {
-  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+  const { isAuthenticated, userId } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const { data: userCvs, isLoading } = useGetCvsQuery();
 
   if (!isAuthenticated) {
     return <Navigate to="/" />;
   }
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const existingCV = userCvs?.some((cv) => cv.user === userId);
+
+  if (existingCV) {
+    return (
+      <div>
+        <p>
+          You already have a CV associated with your account. You can view or
+          edit it <Navigate to="/cv-list" replace />.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <CreateCV />
-      {/* CV skjema her */}
+      {/* Render the CreateCV form only if user does not already have a CV */}
     </div>
   );
 };
